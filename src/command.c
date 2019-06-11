575ce7658a62941e6520589b3c6131d7cc9a1ca9
doorumie92 <doorumiee@gmail.com>
2019-06-11 20:14:30 +0900

    command

--- src/command.c
+++ src/command.c
@@ -19,36 +19,55 @@ void strDis()
 {
     char str[MAX_COMMAND];
     fgets(str,MAX_COMMAND,stdin);
+    if(strlen(str)!=0)
+        str[strlen(str)-1]='\0';
     char* comm[100] = {NULL,};
     int i=0;
 
-    char *temp = strtok(str," ");
-    while(temp != NULL)
-    {
-        comm[i] = temp;
-        i++;
-        temp = strtok(NULL, " ");
+    if(strlen(str)!=0 && str[0] != ' '){
+        char *temp = strtok(str," ");
+        while(temp != NULL)
+        {
+            comm[i] = temp;
+            i++;
+            temp = strtok(NULL, " ");
+        }
+        command(comm, i);
     }
-
-    command(comm, i);
 }
-
 
 //comm 을 받아서 문자에 맞게 명령어 실행
 void command(char * comm[MAX_COMMAND], int i)
 {
-    if(!strcmp(comm[0],"cdr")){
-        if(i==1) cdr(NULL);
-        else if(i != 2 )
-            printf("usage : cdr <dir name | path> \n");
-        else
-            cdr(comm);
+    if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"lst"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else if(!strcmp(comm[0],"cdr"))
+    {}
+    else
+    {
+        printf("%s is not command\n",comm[0]);
     }
-}
 
-void cdr(char * comm[MAX_COMMAND])
-{
-    printf("comm1:%s\n",comm[1]);
-    if(chdir(comm[1]))
-        printf("error");
 }
